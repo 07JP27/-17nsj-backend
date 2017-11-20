8d70f852ebd24b6c0dc804da88e7928349206787
07JP27 <jynpei.t@gmail.com>
2017-11-20 22:47:20 +0900

    MoviesControllerの実装

---
+++ 17nsj.Service/Controllers/MoviesController.cs
@@ -0,0 +1,54 @@
+﻿//----------------------------------------------------------------------
+// <copyright file="MoviesController.cs" company="17NSJ PR Dept">
+// Copyright (c) 17NSJ PR Dept. All rights reserved.
+// </copyright>
+// <summary>MoviesControllerクラス</summary>
+//----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+using _17nsj.DataAccess;
+
+namespace _17nsj.Service.Controllers
+{
+    /// <summary>
+    /// MoviesControllerクラス
+    /// </summary>
+    [Authorize]
+    [RoutePrefix("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        /// <summary>
+        /// ムービー情報を取得します。
+        /// </summary>
+        /// <returns>HTTPレスポンス</returns>
+        [HttpGet]
+        [Route("")]
+        public HttpResponseMessage Get()
+        {
+            if (!this.CanRead())
+            {
+                return this.Request.CreateResponse(HttpStatusCode.Forbidden);
+            }
+
+            using (Entities entitiies = new Entities())
+            {
+                var movies = entitiies.Movies.ToList();
+
+                if (movies != null)
+                {
+                    return this.Request.CreateResponse(HttpStatusCode.OK, movies);
+                }
+                else
+                {
+                    return this.Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not found");
+                }
+            }
+        }
+    }
+}
