b0adea83e33826bf13f22365fa0e98b4dc24696c
07JP27 <jynpei.t@gmail.com>
2018-07-31 15:09:45 +0900

    Notonotono

---
+++ 17nsj.Jedi/Pages/Notonotono.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _17nsj.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _17nsj.Jedi.Pages
+{
+    public class NotonotonoModel : PageModelBase
+    {
+        public NotonotonoModel(JediDbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+
+        public IActionResult OnGet()
+        {
+            this.PageInitializeAsync();
+            return this.Page();
+        }
+    }
+}
